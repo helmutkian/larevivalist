5d84e8d61f4cd75c35c797b157164a2a257c5c28
Helmut K Rohrbacher <helmut.rohrbacher@gmail.com>
2014-01-29 15:06:25 -0800

    Can now look up month and date from the page source

--- cinefamily.lisp
+++ cinefamily.lisp
@@ -1,6 +1,9 @@
 (defpackage #:com.larevivalist.scraper.cinefamily
   (:nicknames #:larev.cf)
-  (:use #:cl #:com.larevivalist.scraper.utils)
+  (:use #:cl 
+	#:com.larevivalist.scraper.utils
+	#:hk.utils.thrush
+	#:hk.utils.pred)
   (:export #:scrape-shows))
 
 (in-package #:com.larevivalist.scraper.cinefamily)
@@ -9,8 +12,7 @@
 (defvar *cf-cal-dom* nil)
 
 (defun get-dom (&optional (url *cf-cal-url*))
-  (if *cf-cal-dom*
-      *cf-cal-dom*
+  (or *cf-cal-dom*
       (setf *cf-cal-dom*
 	    (ws:get-processed-dom url))))
 
@@ -34,11 +36,23 @@
   (mapcar #'ws:a-href
 	  (ws:find-all entry :tag :a)))
 
-(defun get-day (entry &key (month *month*) (year *year*))
+(defun get-month-year (dom)
+  (let ((month-year 
+	 (->>
+	     (-> dom
+	       (ws:find-first :attrib '((:id "EC_current-month")))
+	       (ws:find-first :tag :h2)
+	       (ws:get-text))
+	   (mapcar (alexandria:curry #'remove-if #'space-char-p)))))
+    (cons (get-month-num (car month-year))
+	  (cdr month-year))))
+    
+(defun get-day (entry)
   (ws:get-text (ws:find-first entry :class "dayHead")))
 
-(defun format-showtimes (entry)
-  (loop with date = (format-date (get-day entry))
+
+(defun format-showtimes (entry month year)
+  (loop with date = (format-date (get-day entry) :month month :year year)
         for showtime in (get-showtimes entry)
         for link in (get-links entry)
         collect (make-show :title (first showtime) 
@@ -48,10 +62,12 @@
 			   :link link)))
 
 (defun collect-showtimes (cal-dom ignore-past)
-  (loop for entry in (get-calendar-entries cal-dom 
+  (loop with (month year) = (get-month-year cal-dom)
+        for entry in (get-calendar-entries cal-dom 
 					   :ignore-past-entries ignore-past)
-        for shows = (format-showtimes entry)
+        for shows = (format-showtimes entry month year)
         append shows))
 
 (defun scrape-shows (&key (ignore-past-shows t))
-  (collect-showtimes (get-dom) ignore-past-shows))
+  (loop for cal-dom = (get-dom)
+        append (collect-showtimes cal-dom ignore-past-shows)))
