658893d7f4b2d4d8221935ef7d9a0979cf50d4f4
Helmut K Rohrbacher <helmut.rohrbacher@gmail.com>
2014-03-01 16:24:56 -0800

    Moved new python code in

---
+++ python/larevivalist.py
@@ -0,0 +1,33 @@
+class Show:
+    def __init__(self, title, date, time, theatre, link):
+        self.title = title
+        self.date = date
+        self.time = time
+        self.theatre = theatre
+        self.link = link
+
+    def to_csv(self):
+        quote = lambda x: '\"'+x+'\"'
+        csv = quote(self.title + ' @ ' + self.theatre)
+        csv += ',' + self.date
+        csv += ',' + self.time
+        csv += ',' + quote(self.theatre)
+        csv += ',' + quote(self.link)
+        return csv.encode('utf-8')
+
+def get_month_num(month_str):
+    months = ["january","februrary","march","april","may","june","july"\
+             ,"august","september","october","november","december"]
+    return months.index(month_str.lower())+1
+
+def format_date(month,day,year):
+    return str(month)+'/'+str(day)+'/'+str(year)
+
+def write_to_file(shows, file_name):
+    f = open(file_name, 'w')
+    with f:
+        f.write("Subject,Start Date,Start Time,Location,Description\n")
+        for show in shows:
+            f.write(show.to_csv()+'\n')
+
+
