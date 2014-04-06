4071d8b4214aa8fe4e2fe5d9b4f54859020fc161
Helmut K Rohrbacher <helmut.rohrbacher@gmail.com>
2014-04-05 19:57:41 -0700

    Making progress on lb art theatre scraper

--- python/lbat.py
+++ python/lbat.py
@@ -1,3 +1,4 @@
+from larevivalist import *
 from bs4 import BeautifulSoup
 import urllib2
 
@@ -7,16 +8,30 @@ def open_page(url):
     src = urllib2.urlopen(url)
     return BeautifulSoup(src)
 
-def format_date(date_str):
-    month_str, day, year = date_str.split(' ')[1:]
-    month = get_month_num(month_str, map(lambda m: m[0:3], MONTHS))
-    return str(month)+'/'+day+'/'+year
+def format_lbat_date(date_str):
+    month_day, year = date_str[4:].split(", ")
+    month_str, day = month_day.split(' ')
+    month = get_month_num(month_str)
+    return format_date(month, day, year)
 
 def get_calendar_entries(soup):
     entries = []
-    for option in soup.find_all("option"):
-        date = format_date(option.contents[0])
-        page = root_url+'/'+option.get("value")
-        entries.append((date, option))
+    for option in soup.find("select").find_all("option"):
+        date = format_lbat_date(option.contents[0])
+        entry_page = root_url + option.get("value")
+        entries.append((date, entry_page))
+    return entries
 
+def get_showlistings(entry):
+    results = filter(lambda s: s.find('b'), entry.find_all("br")[11:])
+    listing_set = {}
+    for result in results:
+        title = result.find('b').text
+        if not(title in listing_set):
+            listing_set[title] = result
+    return listing_set.values()
 
+def get_showtime(showlisting):
+    title = showlisting.contents[0].contents[0]
+    time = showlisting.contents[1].contents[0] # fix text encoding
+    return (title, time)
